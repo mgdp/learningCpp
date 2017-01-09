d373cc4401dab834d657dee1ac7b31a67693d2ce
mgdp <marilu.dickperez@gmail.com>
2017-01-09 14:43:20 -0600

    add exercises 0-7 and 0-8

---
+++ Ch0/0-7_8/hw.cpp
@@ -0,0 +1,11 @@
+// a small c++ program
+#include <iostream>
+
+int main()
+{
+    // this is a comment that extends ove rseveral lines 
+    // by using // at th ebeginning of each line instead of using /*
+    // or */ to delimit comments.
+    std::cout << "Does this work?" << std::endl;
+    return 0;
+}
---
+++ Ch0/0-7_8/multLineComnt-7.cpp
@@ -0,0 +1,10 @@
+// a small c++ program
+#include <iostream>
+
+int main()
+{
+    /* this is a comment that extends ove rseveral lines 
+       because it uses /\* and *\/ as starting and ending delimiters. */
+    std::cout << "How about this, does this work?" << std::endl;
+    return 0;
+}
