01c8a808160f6741604389bf886bd88d0e560663
mgdp <marilu.dickperez@gmail.com>
2017-01-09 13:52:49 -0600

    add exercises 0-3 and 0-4

---
+++ Ch1/0-3/tabs.cpp
@@ -0,0 +1,8 @@
+// a small c++ program
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello, \tworld!" << std::endl;
+    return 0;
+}
---
+++ Ch1/0-4/hwProg.cpp
@@ -0,0 +1,8 @@
+// a small c++ program
+#include <iostream>
+
+int main()
+{
+    std::cout << "int main()\n{\n    std::cout << \"Hello, world!\" << std::endl;\n    return 0;\n}" << std::endl;
+    return 0;
+}
