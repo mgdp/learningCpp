1a78f8ff2ae6840d3d4947880a4b64ed49cad675
mgdp <marilu.dickperez@gmail.com>
2017-01-10 14:44:41 -0600

    update exercise 1-1, explain associativity in comments

--- Ch1/1-1/hello_world_stringliterals.cpp
+++ Ch1/1-1/hello_world_stringliterals.cpp
@@ -1,4 +1,10 @@
-// show that you can't concatenate two string literals.
+// This works because you're not concatenating two string literals because
+// the operator: + , is left-left associative, meaning:
+//    const std::string greeting = hello + ", world" + "!!";
+//    is the same as:
+//    const std::string greeting = (hello + ", world") + "!!";
+//    where the object in the parentheses is no longer a "string literal" but
+//    a "string".
 #include <iostream>
 #include <string> 
 
@@ -6,7 +12,7 @@ int main()
 {
     const std::string hello = "Hello";  
     // build the message that we intend to write
-    const std::string greeting = hello + ", world" + "!!";  // show that you can't concatenate two string literals.
+    const std::string greeting = hello + ", world" + "!!";
 
     // write all
     std::cout << std::endl;
