f586444f2e2b7bec00fb388f0186911c649818a4
mgdp <marilu.dickperez@gmail.com>
2017-01-10 13:48:38 -0600

    add section 1.1 programs, add general README

---
+++ Ch1/1.1/helloName_fancy.cpp
@@ -0,0 +1,39 @@
+// ask for a person's name and generate a framed greeting
+// Like:
+// 1 ****************
+// 2 *              *
+// 3 * Hello, Name! *
+// 4 *              *
+// 5 ****************
+#include <iostream>
+#include <string> 
+
+int main()
+{
+    // ask for the person's name
+    std::cout << "Please enter your first name: ";
+
+    // read the name
+    std::string name;    // define -name-
+    std::cin >> name;    // read into -name-
+    
+    // build the message that we intend to write
+    const std::string greeting = "Hello, " + name + "!!"; 
+
+    // build the second and fourth lines of the output
+    const std::string spaces(greeting.size(), ' '); 
+    const std::string second = "* " + spaces + " *";
+
+    // build the first and fifth lines of the output
+    const std::string first(second.size(), '*');
+
+    // write all
+    std::cout << std::endl;
+    std::cout << first << std::endl;
+    std::cout << second << std::endl;
+    std::cout << "* " << greeting << " *" << std::endl;
+    std::cout << second << std::endl;
+    std::cout << first << std::endl;
+
+    return 0;
+}
