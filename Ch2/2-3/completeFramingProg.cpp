9eeb5f98c2b919fe9746ec7842acec9853af8fe2
mgdp <marilu.dickperez@gmail.com>
2017-01-13 16:09:50 -0600

    add exercise 2-3, ask user for size of padding around greeting

---
+++ Ch2/2-3/completeFramingProg.cpp
@@ -0,0 +1,68 @@
+// change the padding from static to user input
+#include <iostream>
+#include <string>
+
+// say what standard-library names we use
+using std::cin;		using std::endl;
+using std::cout; 	using std::string;
+
+int main()
+{
+    // ask for the person's name
+    cout << "Please enter your first name: ";
+
+    // read the name 
+    string name;
+    cin >> name;
+    
+    // ask for the padding size
+    cout << "Please enter how many blank lines/space around your greeting: ";
+
+    // read the name 
+    int pad;
+    cin >> pad;
+
+    // build the message that we intend to write
+    const string greeting = "Hello, " + name + "!";
+    
+    // the number of blanks surrounding the greeting
+//    const int pad = 0;
+    
+    // the number of rows and columns to write
+    const int rows = pad * 2 + 3;
+    const string::size_type cols = greeting.size() + pad * 2 + 2;
+    
+    // write a blank line to separate the output from the input
+    cout << endl;
+    
+    // write _rows_ rows of output
+    // invariant: we have written _r_ rows so far
+    for (int r = 0; r != rows; ++r) {
+    
+        string::size_type c = 0;
+    
+        // invariant: we have written _c_ characters so far in the current row
+        while (c != cols) {
+
+           // is it time to write the greeting?
+           if (r == pad + 1 && c == pad + 1) {
+               cout << greeting;
+               c += greeting.size();
+           } else {
+
+               // are we on the border?
+               if (r == 0 || r == rows - 1 ||
+                   c == 0 || c == cols - 1)
+                   cout << "*";
+               else
+                   cout << " ";
+               ++c;
+            }
+        }
+
+        cout << endl; 
+    }   
+ 
+    return 0;
+
+}
